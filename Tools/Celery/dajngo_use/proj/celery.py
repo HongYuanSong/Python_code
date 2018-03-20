fb6c510a4214850b3541c5eebbad035342d4f836
shy <songhongyuan920407@163.com>
2018-03-20 11:00:33 +0800

    celery

---
+++ Tools/Celery/dajngo_use/proj/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+from django.conf import settings
+
+
+# 为celery程序设置DJANGO_SETTINGS_MODULE环境变量
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro_name.settings')
+
+app = Celery('pro_name')
+
+# 从Django的设置文件中导入CELERY设置
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# 从所有已注册的app中加载任务模块
+app.autodiscover_tasks(lambda:settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
---
+++ Tools/Celery/periodic_task/tasks.py
@@ -0,0 +1,57 @@
+__author__ = 'shy'
+__date__ = '2018/3/20 10:19'
+
+from celery import Celery
+from celery.schedules import crontab
+
+
+app = Celery()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+
+@app.task
+def test(arg):
+    print(arg)
+
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+##########通过配置文件创建定时任务##########
+
+app.conf.beat_schedule = {
+    # Executes tasks.add every 30 seconds
+    'add-every-30-seconds': {
+        'task': 'tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
+
+
+app.conf.beat_schedule = {
+    # Executes tasks.add every Monday morning at 7:30 a.m
+    'add-every-monday-morning': {
+        'task': 'tasks.add',
+        'schedule': crontab(hour=7, minute=30, day_of_week=1),
+        'args': (16, 16),
+    },
+}
+
+app.conf.timezone = 'UTC'
