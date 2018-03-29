a7f15e416e70ea2f80faf63d7d9a1bb6477fdab5
shy <songhongyuan920407@163.com>
2018-03-29 13:59:44 +0800

    tornado_doc

--- Tornado_test/user_guide/异步接口.py
+++ Tornado_test/user_guide/异步接口.py
@@ -1,6 +1,7 @@
 __author__ = 'shy'
 __date__ = '2018/3/29 9:45'
 
+import tornado.web
 from tornado.httpclient import HTTPClient
 
 
@@ -9,6 +10,12 @@ def synchronous_fetch(url):
     http_client = HTTPClient()
     response = http_client.fetch(url)
     return response.body
+
+
+# *****tornado异步请求实现方式*****
+# 1. callback参数
+# 2. Future对象
+# 3. 协程模式
 
 
 # 通过回调参数callback进行异步调用
@@ -37,6 +44,15 @@ def async_fetch_future(url):
     return my_future
 
 
+# 通过asynchronous可将请求变为长链接，只有手动调用self.finish()才会响应
+
+class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.write("Hello, world")
+        self.finish()
+
+
 # 通过协程进行异步请求
 from tornado import gen
 
@@ -47,6 +63,9 @@ def fetch_coroutine(url):
     response = yield http_client.fetch(url)
     # raise gen.Return(response.body) # python2
     return response.body # python3.3
+
+
+
 
 
 if __name__ == '__main__':
